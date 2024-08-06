65b31c78a003d4a16caa48240da249a6ca461ee6
PachaiyappanV <pachaiyappan394@gmail.com>
2024-08-06 17:11:31 +0530

    feat: add fetch tours function

--- 01-Tours/src/App.jsx
+++ 01-Tours/src/App.jsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
+
 const url = "https://www.course-api.com/react-tours-project";
 
 const App = () => {
+  const [tours, setTours] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchTours = async () => {
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      console.table(tours);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
   return <h2>Tours Starter</h2>;
 };
 export default App;
