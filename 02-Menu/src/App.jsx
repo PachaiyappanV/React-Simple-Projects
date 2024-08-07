e2ebd4dcc234d33b55411b15179f041955a447de
PachaiyappanV <pachaiyappan394@gmail.com>
2024-08-07 12:08:48 +0530

    feat: add menu and menu item component

--- 02-Menu/src/App.jsx
+++ 02-Menu/src/App.jsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
+import Menu from "./Menu.jsx";
+import Title from "./Title";
+import data from "./data.js";
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
+  const [menuItems, setMenuItems] = useState(data);
+  return (
+    <section className="menu">
+      <Title text="our menu" />
+      <Menu items={menuItems} />
+    </section>
+  );
 };
 export default App;
--- 02-Menu/src/Title.jsx
+++ 02-Menu/src/Title.jsx
@@ -2,7 +2,6 @@ const Title = ({ text }) => {
   return (
     <div className="title">
       <h2>{text}</h2>
-      <div className="title-underline"></div>
     </div>
   );
 };
