01b570d2cd315fc93cb22e7c9d3ee3bf8a051aed
PachaiyappanV <pachaiyappan394@gmail.com>
2024-08-09 12:11:13 +0530

    feat : add dark-theme state to local storage and use useEffect

--- 03-Unsplash/src/context.jsx
+++ 03-Unsplash/src/context.jsx
@@ -1,15 +1,20 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 const AppContext = createContext();
 const AppProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(
+    localStorage.getItem("dark-theme") === "true" ? true : false
+  );
   const [searchTerm, setSearchTerm] = useState("tiger");
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme;
     setIsDarkTheme(newDarkTheme);
-    document.body.classList.toggle("dark-theme", newDarkTheme);
+    localStorage.setItem("dark-theme", newDarkTheme);
   };
 
+  useEffect(() => {
+    document.body.classList.toggle("dark-theme", isDarkTheme);
+  }, [isDarkTheme]);
   return (
     <AppContext.Provider
       value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}
