8a61fab286c37768d0caadb446a55d1b4627233b
PachaiyappanV <pachaiyappan394@gmail.com>
2024-08-09 23:34:06 +0530

    feat: add react router

--- 04-MixMaster/src/App.jsx
+++ 04-MixMaster/src/App.jsx
@@ -1,4 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { About, HomeLayout } from "./pages";
 const App = () => {
-  return <h2>mixmaster starter</h2>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeLayout />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 export default App;
