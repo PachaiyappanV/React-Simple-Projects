41c2a6340096470fbf2f02fb47a994ee2985d49a
PachaiyappanV <pachaiyappan394@gmail.com>
2024-10-29 00:00:10 +0530

    feat: add accordian

--- 00-React-interview-questions/src/App.jsx
+++ 00-React-interview-questions/src/App.jsx
@@ -1,7 +1,3 @@
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline text-amber-400">
-      Hello world!
-    </h1>
-  );
+  return <h1 className="text-3xl font-bold underline text-amber-400"></h1>;
 }
